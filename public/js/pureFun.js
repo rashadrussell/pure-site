02ed780ff3f042f5b57ca0e0403668b513793287
Rashad Russell <rashadr@yahoo-inc.com>
2013-05-31 11:53:18 -0700

    Added comments to the pureFun.js file

--- public/js/pureFun.js
+++ public/js/pureFun.js
@@ -1,11 +1,12 @@
 YUI().use('node', 'transition', function(Y) {
 	
+	// A simple script that displays a description of the divs on mouseover
 	var divs = Y.all('.pureFun > div');
 		
 	divs.each(function(){
 		
 		this.on('mouseover', function(){
-			this.get('children').item(1).transition({
+			this.get('children').item(1).transition({ // item(1) is the <p> element used to contain the description
 				delay: 0,
 				duration: 0.5,
 				ease: 'ease-in',
@@ -14,7 +15,7 @@ YUI().use('node', 'transition', function(Y) {
 		});
 		
 		this.on('mouseout', function(){
-			this.get('children').item(1).transition({
+			this.get('children').item(1).transition({ // item(1) is the <p> element used to contain the description
 				delay: 0,
 				duration: 1.0,
 				ease: 'ease-in',
