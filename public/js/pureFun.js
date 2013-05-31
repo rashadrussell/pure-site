8732db9ab0889cbd42101f874dc1c52dd8f12faa
Rashad Russell <rashadr@yahoo-inc.com>
2013-05-31 11:09:35 -0700

    Created a pureFun page to display all of the creative things users could make with Pure

---
+++ public/js/pureFun.js
@@ -0,0 +1,27 @@
+YUI().use('node', 'transition', function(Y) {
+	
+	var divs = Y.all('.pureFun > div');
+		
+	divs.each(function(){
+		
+		this.on('mouseover', function(){
+			this.get('children').item(1).transition({
+				delay: 0,
+				duration: 1.0,
+				ease: 'ease-in',
+				bottom: '0px'
+			});
+		});
+		
+		this.on('mouseout', function(){
+			this.get('children').item(1).transition({
+				delay: 0,
+				duration: 2.0,
+				ease: 'ease-in',
+				bottom: '-100px'
+			});
+		});
+		
+	});
+	
+});
